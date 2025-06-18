4e1d8f959a0b343178f045414dc2781c3ce8172c
Dishangohil <dishagohil987@gmail.com>
2025-06-18 22:08:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ app.secret_key = 'mysecretkey'  # CSRF protection key
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Here you can add code to save the user info to a database
         flash(f"Successfully Registered! Name: {form.name.data}, Email: {form.email.data}", "success")
         return redirect(url_for('register'))
     return render_template("reg.html", form=form)
 
 if __name__ == "__main__":
-   app.run(debug=False, use_reloader=False, port=5003)
-    port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    # Get port from environment variable or default to 5003
+    port = int(os.environ.get("PORT", 5003))
+    app.run(host='0.0.0.0', port=port)
